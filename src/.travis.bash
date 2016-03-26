a16b325053c02783ae8f33414adb7a72e323fee5
jaswsinc <jaswsinc@wsharks.com>
2016-03-25 19:51:59 -0800

    Bug fix. Should `.travis.bash`.

--- src/.travis.bash
+++ src/.travis.bash
@@ -5,4 +5,4 @@ set -o errexit;
 mkdir --parents ~/ws/repos &>/dev/null;
 git clone https://github.com/websharks/travis-ci ~/ws/repos/travis-ci --branch=master --depth=1 &>/dev/null;
 
-~/ws/repos/travis-ci/src/.bash;
+~/ws/repos/travis-ci/src/.travis.bash;
