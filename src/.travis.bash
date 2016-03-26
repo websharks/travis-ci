ff9f604bc8b25d30556f94ac3efef97d492b2995
jaswsinc <jaswsinc@wsharks.com>
2016-03-25 19:34:15 -0800

    Use master branch.

--- src/.travis.bash
+++ src/.travis.bash
@@ -3,6 +3,6 @@
 set -o errexit;
 
 mkdir --parents ~/ws/repos &>/dev/null;
-git clone https://github.com/websharks/travis-ci ~/ws/repos/travis-ci &>/dev/null;
+git clone https://github.com/websharks/travis-ci ~/ws/repos/travis-ci --branch=master --depth=1 &>/dev/null;
 
 ~/ws/repos/travis-ci/.bash;
