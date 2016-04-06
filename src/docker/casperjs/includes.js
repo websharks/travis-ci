c92cd7dccaabda27beae7cc410996e171a7c5be6
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 09:51:29 -0800

    Cleanup includes.

--- src/docker/casperjs/includes.js
+++ src/docker/casperjs/includes.js
@@ -2,10 +2,9 @@ $ = casper; // Casper alias.
 $$ = {}; // Custom extensions.
 
 $$.system = require('system');
-
-require('/bootstrap-ci/src/docker/casperjs/other/aliases.js');
-require('/bootstrap-ci/src/docker/casperjs/events/resources.js');
-
 _ = require('/bootstrap-ci/src/docker/casperjs/libs/underscore.js');
 $$.www = require('/bootstrap-ci/src/docker/casperjs/libs/www.js');
 $$.wp = require('/bootstrap-ci/src/docker/casperjs/libs/wp.js');
+
+require('/bootstrap-ci/src/docker/casperjs/events/resources.js');
+require('/bootstrap-ci/src/docker/casperjs/other/aliases.js');
